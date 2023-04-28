adb754418dab073e1a3da8ab9925ce5ca7a54477
Valery Filatov <valiklfiatoff@yandex.ru>
2023-04-28 19:32:37 +0300

    database update

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///eureka.db'
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nick = db.Column(db.String(16), nullable=False)
+    email = db.Column(db.String, nullable=False)
+    name = db.Column(db.String)
+    second_name = db.Column(db.String)
+    about = db.Column(db.Text(500))
+
+    def __repr__(self):
+        return self.nick
 
 
 @app.route('/')
