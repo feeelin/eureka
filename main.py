04fbd7b91e964fe1efc55a81a4c632b8d5614564
Valery Filatov <valiklfiatoff@yandex.ru>
2023-04-28 18:58:53 +0300

    base for html

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+from flask import Flask, render_template
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = Flask(__name__)
 
 
-# Press the green button in the gutter to run the script.
+@app.route('/')
+def main():
+    return render_template('main.html', title='Главная страница')
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run(debug=True)
